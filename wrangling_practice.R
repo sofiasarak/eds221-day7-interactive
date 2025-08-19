e0002c9cc597b11ab63071488972a439c3a1f4c5
sofiasarak <sofiasarak30@gmail.com>
2025-08-19 11:46:35 -0700

    practing joins

--- wrangling_practice.R
+++ wrangling_practice.R
@@ -27,8 +27,60 @@ penguinsx <- penguins |>
 penguins_summary <- penguins |>
   filter(species == "Adelie") %>% #could also include the next row in the same (), sep by a comma
   filter(!is.na(flipper_length_mm)) %>% #keep rows that are not NA
-  group_by(sex) %>%
+  group_by(sex) %>% #group_by doesn't save anything; is just used with other operations
   summarize(mean = mean(flipper_length_mm, na.rm = TRUE),
             sd = sd(flipper_length_mm, na.rm = TRUE),
             sample_size = n()) #n() doesn't require an argument bc it's just counting obvs
 
+#pasting in example data sets using datapasta!
+# Tools > Addins > Browse Addins > paste as data.frame (from datapasta)
+
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+           species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+          maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+)
+
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+
+library(dplyr)
+
+#Mutating Joins
+
+# practice with a full_join(), everything is kept! (keeps all rows and adds all columns)
+full_join(animals, sites) #arguments are data frames
+
+#left_join(); keep first data frame basically the same, and add on new columns from sites
+left_join(animals, sites)
+
+#right_join(); the opposite, will keep four rows from sites (but also an extra from many of many)
+right_join(animals, sites)
+
+#inner_join(); only keep rows where key matches in both x and y; never any NAs bc everything will match up
+inner_join(animals, sites)
+
+#note: can only join two tables at one time!
+
+#Filtering Joins
+#don't add on any columns! and order really matters
+
+#semi_join()
+semi_join(animals, sites)
+#which is this the same as this code:
+animals %>%
+  filter(location %in% sites$location)
+
+anti_join(animals, sites)
+#which is same as:
+animals %>%
+  filter(!location %in% sites$location)
+
+semi_join(sites, animals)
